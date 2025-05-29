61d0f35b118215a98602115b2c120e46020ff379
karivone <karimiyvonne19@gmail.com>
2025-05-29 13:56:32 +0300

    messaging app

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
