f8f7e7f6f9161ad555686dc9c45f0385dcdc4f0b
karivone <karimiyvonne19@gmail.com>
2025-05-29 08:53:59 +0300

    Unit Tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,6 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from fixtures import org_payload
-
 from client import GithubOrgClient  # Ensure this matches your actual file structure
 
 
@@ -17,17 +15,14 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")  # Patch the get_json used in the GithubOrgClient module
     def test_org(self, org_name, mock_get_json):
         """Test that GithubOrgClient.org returns the correct data"""
-        # Arrange
         expected_result = {"login": org_name, "id": 1234}
         mock_get_json.return_value = expected_result
 
-        # Act
         client = GithubOrgClient(org_name)
-        result = client.org
-
-        # Assert
-        self.assertEqual(result, expected_result)
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(client.org, expected_result)
+        mock_get_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+        )
 
 
 if __name__ == '__main__':
