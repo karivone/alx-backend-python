e6518cc82d8290262e844b1357ad3af72825f6f6
karivone <karimiyvonne19@gmail.com>
2025-05-29 10:38:29 +0300

    Unit Tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from unittest.mock import patch, Mock
-from parameterized import parameterized_class
+from parameterized import parameterized, parameterized_class
 
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
