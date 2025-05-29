e253b42cdf1a2fce0a898ed889b0b1e20782d419
karivone <karimiyvonne19@gmail.com>
2025-05-29 14:07:44 +0300

    messaging app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
+router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
